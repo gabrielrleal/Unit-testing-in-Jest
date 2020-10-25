ffd1ce7e3f64c11c3b363425b39df663daaf0452
Gabriel <gabrielxleal@gmail.com>
2020-10-25 14:48:45 -0300

    start using matchers to understand how test works

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@ const functions = {
     getAge:  (name, age) => {
         return `${name} is ${age} years old`
 
-    }
+    },
+    add: (num1, num2) => num1 + num2
 }
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,7 +1,17 @@
 const functions = require('./functions');
 
-// descrio do que teste faz
+// describes the test
 test('is defined', () => {
-    expect(functions.getAge("Rudi", 35)).toBeDefined() //
+    expect(functions.getAge("Rudi", 35)).toBeDefined() 
 
+})
+
+test('2 + 2 is equal to 4', () => {
+    expect(functions.add(2,2)).toBe(4)//matching, toBe used to test primitive values
+})
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2
+    expect(data).toEqual({one:1, two:2})//toEqual, checks objects
 })
